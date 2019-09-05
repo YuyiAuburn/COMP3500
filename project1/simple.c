28a0d67e98e93d6db240a7f3d94aadbf5fd62a73
Yueyi Gao <yzg0032@tux053.eng.auburn.edu>
2019-09-04 20:50:37 -0500

    Initial

---
+++ project1/simple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+	int sample[10] = {1,4,9,16,25,36,49,64,81,100};
+	double average = 0;
+	int sum = 0;
+	int i;
+	for (i = 0; i <10; i++) {
+		sum = sum + sqrt(sample[i]);
+	}
+	average = (float)sum/10;
+	printf("Average is: %.2f\n",average);
+	return 0;
+}
